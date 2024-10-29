73e4e1f27626872adc78251794e35b50af1fe27b
zewais <arch.ziad.ewais@gmail.com>
2024-10-29 09:15:38 -0400

    add signup form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import SignUpForm from "./Components/SignUpForm";
 import TodoAppContainer from "./Components/TodoAppContainer";
 
 function App() {
   return (
     <>
+      <SignUpForm />
       <TodoAppContainer />
     </>
   );
