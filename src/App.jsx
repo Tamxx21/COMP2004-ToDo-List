09d31d371f0283bd226c3ef082dea2753cdc9d28
zewais <arch.ziad.ewais@gmail.com>
2024-10-28 22:37:14 -0400

    todo list

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TodoAppContainer from "./Components/TodoAppContainer";
+
+function App() {
+  return (
+    <>
+      <TodoAppContainer />
+    </>
+  );
+}
+
+export default App;
